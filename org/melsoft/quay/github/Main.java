bb473114649b903ce7c3812abc4b37a9445e9ee0
atlantis <messages.one@outlook.com>
2021-12-19 23:10:52 +0530

    add a simple Java console app

---
+++ org/melsoft/quay/github/Main.java
@@ -0,0 +1,11 @@
+package org.melsoft.quay.github;
+
+public class Main {
+    public Main() {
+        System.out.println("Hello Quay Github Integration Demo");
+    }
+
+    public static void main( String ...args) {
+        new Main();
+    }
+}
